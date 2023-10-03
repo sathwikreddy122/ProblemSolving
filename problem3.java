d030f80d52ab9243df756aea12e7ad06274ecc3a
sathwikreddy122 <venkatasathwik12604@gmail.com>
2023-10-03 22:59:47 +0530

    "Using for loop"

--- problem3.java
+++ problem3.java
@@ -5,14 +5,19 @@ public class problem3 {
         //reverse an array
         int[] arr = {4,5,1,2,3};
         int n = arr.length;
-        int start = 0;
-        int end = n-1;
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+        // int start = 0;
+        // int end = n-1;
+        // while(start < end){
+        //     int temp = arr[start];
+        //     arr[start] = arr[end];
+        //     arr[end] = temp;
+        //     start++;
+        //     end--;
+        // }
+        for(int i = 0; i < n/2; i++){
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
         }
         System.out.println(Arrays.toString(arr));
     }
