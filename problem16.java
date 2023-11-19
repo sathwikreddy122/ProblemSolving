183d402390261030be1db48fe74bdd44a4f67873
sathwikreddy122 <venkatasathwik12604@gmail.com>
2023-11-20 00:18:47 +0530

    Right angled triangle pattern

---
+++ problem16.java
@@ -0,0 +1,12 @@
+public class problem16 {
+    public static void main(String[] args) {
+        //Pattern Program 1.
+        int n = 5;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
