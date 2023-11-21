6e754b55824b84348ffb79d42336787336112744
sathwikreddy122 <venkatasathwik12604@gmail.com>
2023-11-21 10:15:45 +0530

    Pattern problem with #

---
+++ problem20.java
@@ -0,0 +1,23 @@
+public class problem20 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            if(i == 1 || i == n){
+                for(int j = 1; j <= n; j++){
+                    System.out.print("* ");
+                }
+            }
+            else{
+                for(int k = 1; k <= n; k++){
+                    if(k == 1 || k == n){
+                        System.out.print("* ");
+                    }
+                    else{
+                        System.out.print("# ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
