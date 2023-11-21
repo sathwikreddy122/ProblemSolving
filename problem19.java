249be3c012b57c512216613d80902acefef80696
sathwikreddy122 <venkatasathwik12604@gmail.com>
2023-11-21 09:56:01 +0530

    Square pattern

---
+++ problem19.java
@@ -0,0 +1,24 @@
+public class problem19 {
+    public static void main(String[] args) {
+        //Pattern Program 4.
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            if(i == 1 || i == n){
+                for(int j = 1; j <= n; j++){
+                    System.out.print("* ");
+                }
+            }
+            else{
+                for(int k = 1; k <= n; k++){
+                    if(k == 1 || k == n){
+                        System.out.print("* ");
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
